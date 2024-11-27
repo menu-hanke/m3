e08c5b4ed83706680358b7b04de0c6a5121c98f0
Tapio Lempinen <tapio.lempinen@luke.fi>
2024-11-27 03:15:59 +0200

    release: strip fhk dll

--- scripts/release-windows.sh
+++ scripts/release-windows.sh
@@ -5,5 +5,6 @@ if [ "$GITHUB_ACTIONS" == "true" ]; then
 fi
 make -C LuaJIT amalg -j4
 (cd fhk5 && RUSTFLAGS='-L ../LuaJIT/src' cargo build --release -F trace --target x86_64-pc-windows-gnu)
+strip fhk5/target/x86_64-pc-windows-gnu/release/fhk.dll
 make M3EXE_FHK=fhk5/target/x86_64-pc-windows-gnu/release/fhk.dll -j4
 zip -j m3-$(git describe)-x64-windows.zip m3.exe LuaJIT/src/lua51.dll fhk5/target/x86_64-pc-windows-gnu/release/fhk.dll
